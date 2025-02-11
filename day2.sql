b525c0a1831163ac2f80ca14b0156197919323ab
nitishrudra3510@gmail.com <nitishrudra3510@gmail.com>
2025-02-11 11:49:39 +0530

    day2

--- day2.sql
+++ day2.sql
@@ -98,9 +98,141 @@ SELECT * FROM campusx.smartphones where rating > 80 and price < 25000 AND proces
 
 SELECT * FROM campusx.smartphones where brand_name='samsung' AND ram_capacity > 8;
 
-
+ 
 -- find the all samsung phoens and snapdragon processor
 
 SELECT * FROM campusx.smartphones where brand_name='samsung' AND processor_brand = 'snapdragon';
 
 -- # QUery Execution Orders : infytq.onwingspan.com/ websites name 
+-- execution order by FROM JOIN WHERE GROUP BY HAVING SELECT DISTICT ORDER BY 
+-- (FJWGHSDO)
+
+-- FIND THE brands who sell phones with price > 50000
+
+SELECT DISTINCT(brand_name) FROM campusx.smartphones where price > 100000;	
+
+-- IN and NOT IN
+
+select * from campusx.smartphones
+where processor_brand = 'snapdragon' OR
+processor_brand = 'exynos' OR
+processor_brand = 'bionic';
+
+-- easy way 
+select * from campusx.smartphones
+where processor_brand IN ('snapdragon', 'exynos', 'bionic');
+
+select * from campusx.smartphones
+where processor_brand NOT IN ('snapdragon', 'exynos', 'bionic');
+
+
+
+# UPDATE : update karna in the row..
+select * from campusx.smartphones
+where processor_brand = 'mediatek';
+
+--  mediatek ke jagah pr dimensity
+UPDATE campusx.smartphones
+SET processor_brand = 'dimensity'
+where processor_brand = 'mediatek';
+
+
+-- @gmail.com ke jagah pr @yahoo.com..
+UPDATE campusx.users
+SET email = 'swayam@yahoo.com'
+where name = 'swayam';
+
+-- DELETE THOSE ROWS WHOSE PRICE GREATER THAN 200000
+DELETE FROM campusx.smartphones
+where price > 200000;
+
+-- Here will be show error.
+SELECT * FROM campusx.smartphones
+where price > 200000;
+
+-- battery > 70000
+SELECT * FROM campusx.smartphones
+where primary_camera_rear > 150;
+
+-- delete primary_camera_rear > 150 with samsuung
+delete FROM campusx.smartphones
+where primary_camera_rear > 150 and brand_name='samsung';
+
+Select * FROM campusx.smartphones
+where primary_camera_rear > 150 and brand_name='samsung';
+			
+-- ## FUNCTIONS : 
+
+-- find max values in the price 
+
+select MAX(price) FROM campusx.smartphones;
+
+-- min
+select MIN(price) FROM campusx.smartphones;
+
+
+-- find the price of the costlist samsung phone..
+SELECT * FROM campusx.smartphones
+where brand_name = "samsung" AND price = "110999";
+
+-- AVG : 
+SELECT AVG(rating) FROM campusx.smartphones
+WHERE brand_name = 'apple';
+
+-- SUM : 
+select sum(price) from campusx.smartphones;
+
+
+-- count : apple ka phone kitna hai..
+select COUNT(*) from campusx.smartphones
+WHERE brand_name = 'apple';
+
+-- Distinct(count) : NO OF BRAND_NAME
+
+SELECT count(DISTINCT(brand_name)) from campusx.smartphones;
+
+-- STD 
+
+select STD(screen_size) from campusx.smartphones;
+
+-- VARIANCE
+select variance(screen_size) from campusx.smartphones;
+
+
+-- # Scaler functions
+-- ABS 
+SELECT ABS(price - 100000) AS 'temp' FROM campusx.smartphones;
+
+-- ROUND : FIND THE PPI WITH 2 DECIMAL
+SELECT model, 
+ROUND(SQRT(resolution_width*resolution_width + resolution_height*resolution_height)/screen_size, 2) AS 'PPI'
+FROM campusx.smartphones;
+
+
+-- CEILING 
+-- 4.1 -> 5
+-- 5.9 -> 6
+
+-- FLOOR -
+-- 5.5 - 4
+-- 6.9 - 6
+
+select ceil(screen_size) from campusx.smartphones;
+select FLOOR(screen_size) from campusx.smartphones;
+
+
+-- ** Find the average battery capacity and the average primary rear camera resolution for all smartphones with a price greater than or equal to 100000
+SELECT AVG(battery_capacity) AS 'AVG battery_capacity' , AVG(primary_camera_rear) AS 'Avg_primary_camera_rear' FROM campusx.smartphones
+where price >= 100000;
+
+SELECT * FROM campusx.smartphones;
+-- find the number of smartphoens with 5g capability..
+SELECT count(brand_name) FROM campusx.smartphones where has_5g='True';
+
+-- ** Find the average internal memory capacity of smartphones that have a refresh rate of 120 Hz or higher and a front-facing camera
+-- resolution greater than or equal to 20 megapixels.
+-- resolution greater than or equal to 20 megapixels.
+SELECT AVG(internal_memory) 
+FROM campusx.smartphone 
+WHERE refresh_rate >= 120 
+AND front_camera_resolution >= 20;
