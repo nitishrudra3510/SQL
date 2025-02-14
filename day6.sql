a2520c93ef4660c2a67a114c5e2442fe6d3707fb
nitishrudra3510@gmail.com <nitishrudra3510@gmail.com>
2025-02-14 20:55:42 +0530

    day6

--- day6.sql
+++ day6.sql
@@ -90,3 +90,91 @@ from marks;
 
 
 use zomato;
+
+-- per month ke top 2 customer kon hai jo jyada kharch kiya hai..
+
+SELECT date,MONTH(date), MONTHNAME(DATE) FROM orders;
+
+SELECT *
+FROM (
+    SELECT 
+        MONTHNAME(date) AS month_name,
+        MONTH(date) AS month_num,
+        user_id,
+        SUM(amount) AS total_amount,
+        RANK() OVER(PARTITION BY MONTHNAME(date) ORDER BY SUM(amount) DESC) AS month_rank
+    FROM orders
+    GROUP BY MONTHNAME(date), MONTH(date), user_id
+) t1
+WHERE t1.month_num > 3
+ORDER BY t1.month_num ASC, t1.month_rank ASC;
+
+
+-- FIRST_VALUE/ LAST_VALUE & NTH values
+
+-- first_value
+use campusx;
+SELECT *,
+FIRST_VALUE(name) OVER(ORDER BY marks DESC) from marks ;
+
+-- last_value : ye rule se output deta hai unbounded preceding(1st row) and unbounded floowing (last row)
+-- follows the frame rule -> tables -> group -> frame
+SELECT *,
+LAST_VALUE(marks) OVER(PARTITION BY branch
+					ORDER BY marks DESC
+					ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) 
+					from marks ;
+                    
+                    
+-- NTH VAlues..direct posotion nikal sakte hai..
+SELECT *,
+NTH_VALUE(name, 2) OVER(PARTITION BY branch
+					ORDER BY marks DESC
+					ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) 
+					from marks;
+                    
+                    
+-- find the branch toppers 
+
+SELECT name, branch, marks FROM (SELECT *,
+FIRST_VALUE(name) OVER(PARTITION BY branch ORDER BY marks DESC) AS tooper_name,
+FIRST_VALUE(marks) OVER(PARTITION BY branch ORDER BY marks DESC) AS topper_marks
+from marks) t1
+WHERE t1.name = t1.tooper_name AND t1.marks = t1.topper_marks;
+
+-- LEAD & LAG
+
+-- LAG : hamesha ek row piche chalega original values se... or sabse pahela value null ho jayegi
+SELECT *,
+LAG(marks)  over(ORDER BY student_id)
+from marks;
+
+
+-- LEAD : hamesha ek row age chalega original values se... or sabse last value null ho jayegi..
+SELECT *,
+LEAD(marks)  over(ORDER BY student_id)
+from marks;
+
+SELECT *,
+LEAD(marks)  over(PARTITION BY branch ORDER BY student_id)
+from marks;
+
+
+-- find the month on month revenue growth of zomato...alter
+
+USE zomato;
+
+WITH MonthlyRevenue AS (
+    SELECT 
+        MONTH(date) AS month_number, 
+        MONTHNAME(date) AS month_name, 
+        SUM(amount)  AS total_revenue
+    FROM orders
+    GROUP BY MONTH(date), MONTHNAME(date)
+)
+SELECT 
+    month_name, 
+    total_revenue,
+    ((total_revenue - LAG(total_revenue) OVER (ORDER BY month_number))/total_revenue)*100 AS mom_growth
+FROM MonthlyRevenue
+ORDER BY month_number;
